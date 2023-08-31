cb2545ffa1d5123a0cc9792d67c7e10bab855403
allpurposebucket <dinostar237@gmail.com>
2023-08-31 09:13:34 -0500

    Using Unix now

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+
 
 int main(int argc, char *argv[]) {
     return 0;
